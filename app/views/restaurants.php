ff30aa042c4f948b99d55ab9c368468f470c06c6
Tanya Parker <tanyacparker@gmail.com>
2014-03-11 18:09:55 -0700

    cleaned up restaurant view

--- app/views/restaurants.php
+++ app/views/restaurants.php
@@ -9,37 +9,31 @@
 <body>
 	<h1>Yelp</h1>
 
-<!-- 	<?php if(sizeof($dvds) >= 1) {?>
-<table>
+	Restaurant Name (City)
+	Type of Cuisine
+	Facebook Page: link to facebook page
+	link to reviews
 
-	<tr style="font-size: 20px;">
-		<td><b><u>Title</u></b></td>
-		<td><b><u>Rating</u></b></td>
-		<td><b><u>Genre</u></b></td>
-		<td><b><u>Label</u></b></td>
-		<td><b><u>Sound</u></b></td>
-		<td><b><u>Format</u></b></td>
-		<td><b><u>Release Date</b></u></td>
-	</tr>
-	<?php foreach($dvds as $dvd) : ?>
-		<tr>
-			<td><?php echo $dvd->title; ?></td>
-			<td><?php echo $dvd->rating_name; ?></td>
-			<td><?php echo $dvd->genre_name; ?></td>
-			<td><?php echo $dvd->label_name; ?></td>
-			<td><?php echo $dvd->sound_name; ?></td>
-			<td><?php echo $dvd->format_name; ?></td>
-			<td><?php echo $dvd->release_date; ?></td>
-		</tr>
-		
-	<?php endforeach; ?>
-</table>
-<?php
-}
-else {
-	echo "<h1>nothing. Try again!</h1>";
-}
-?> -->
+	<hr>
+
+
+<?php 
+	if(sizeof($restaurants) >= 1) {
+
+	foreach($restaurants as $r) :
+		echo "Restaurant Name (City)";
+		echo "Type of Cuisine";
+		echo "Facebook Page: link to facebook page";
+		echo "link to reviews";
+		//$r->title;
+		echo "<hr>";
+	endforeach;
+	
+	}
+	else {
+		echo "<h1>Sorry. There are no restaurants to display.</h1>";
+	}
+?>
 
 </body>
 </html>
